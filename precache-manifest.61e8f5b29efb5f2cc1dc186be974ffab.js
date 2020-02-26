6fbfb4b3420fa31a2756bd41b653ce4fb73b4479
Greg <ggydush@broadinstitute.org>
2020-02-25 23:49:15 -0500

    Updates

---
+++ precache-manifest.61e8f5b29efb5f2cc1dc186be974ffab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e8f9df7e47d68abe94c265d405669b",
+    "url": "/gameOf24/index.html"
+  },
+  {
+    "revision": "2fcc2075b508a7996ad1",
+    "url": "/gameOf24/static/css/main.5da26137.chunk.css"
+  },
+  {
+    "revision": "e4b123abbfd1f8c2a80f",
+    "url": "/gameOf24/static/js/2.5c79c621.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/gameOf24/static/js/2.5c79c621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcc2075b508a7996ad1",
+    "url": "/gameOf24/static/js/main.8e1a6a1e.chunk.js"
+  },
+  {
+    "revision": "6186a2baa84259afe8ed",
+    "url": "/gameOf24/static/js/runtime-main.a992f744.js"
+  },
+  {
+    "revision": "9185b5c918a8db987136c1813604384b",
+    "url": "/gameOf24/static/media/example.9185b5c9.png"
+  }
+]);
